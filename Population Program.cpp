5c8dcdd5fe6f96e13f55f7ed10b520990379cfd1
jm underwood <jm.underwood@hotmail.com>
2025-07-11 12:58:54 -0400

    First Test

--- Population Program.cpp
+++ Population Program.cpp
@@ -5,5 +5,34 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int starting_org, days, tot_days, population;
+    float increase;
+
+    std::cout << "What is the starting number of organisms? \n";
+    std::cin >> starting_org;
+    
+    if (starting_org < 2)
+    {
+        std::cout << "Value is too low \n";
+        return 0;
+    }
+    std::cout << "What is the average daily increase in population? \n";
+    std::cin >> increase;
+
+    if (increase < 0)
+    {
+        std::cout << "Value is too low \n";
+        return 0;
+    }
+
+    std::cout << "How many days will the organisms be multiplying? \n";
+    std::cin >> tot_days;
+
+    if (tot_days < 1)
+    {
+        std::cout << "Value is too low \n";
+        return 0;
+
+    }
+
 }
